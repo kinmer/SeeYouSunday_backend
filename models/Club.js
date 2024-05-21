f4e3e7127339e0a5d37e43f5e272b6244e553fae
yongZhang <kinmer7@gmail.com>
2024-05-21 13:38:10 -0400

    add event schema

--- models/Club.js
+++ models/Club.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema 
 
+
+const EventSchema = new Schema({
+  topic: String,
+  date: Date,
+  description: String,
+},
+{timestamps: true});
+
+
+
 const ClubSchema = new Schema({
     name: String,
     location: String,
     time: String,
     image: String,
     description: String,
+    events:[EventSchema],
     members: [{
       type: Schema.Types.ObjectId,
       ref: 'Member'
